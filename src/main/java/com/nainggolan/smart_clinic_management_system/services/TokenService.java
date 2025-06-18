62eb3d0b571b026739816bfaebccf943228e9c4f
naingolan <kelvinsdechaw@gmail.com>
2025-06-18 05:21:28 +0300

    changes on token service class

--- src/main/java/com/nainggolan/smart_clinic_management_system/services/TokenService.java
+++ src/main/java/com/nainggolan/smart_clinic_management_system/services/TokenService.java
@@ -1,10 +1,43 @@
 package com.nainggolan.smart_clinic_management_system.services;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class TokenService {
     public boolean validateToken(String token){
         return token !=null && !token.isEmpty();
+    }
+
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${security.jwt.expiration-time}")
+    private long expirationTime; // in milliseconds
+
+    public String generateJwtToken(String email) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("email", email);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(SignatureAlgorithm.HS256, getSigningKey())
+                .compact();
+    }
+
+    public Key getSigningKey() {
+        byte[] keyBytes = secretKey.getBytes();
+        return new SecretKeySpec(keyBytes, SignatureAlgorithm.HS256.getJcaName());
     }
 }
